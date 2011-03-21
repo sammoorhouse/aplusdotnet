4233757ebb1ba20c7ca431021240ca1d09b79024
elecro <elecro@gmail.com>
2011-03-21 16:31:39 +0000

    Fix invoke of function inside function. Generally it was ok, but the optimization didn't take the assignment of a function in account. Test case is also added.

--- src/Aplus/AplusCoreUnitTests/Dlr/InvokeFunction.cs
+++ src/Aplus/AplusCoreUnitTests/Dlr/InvokeFunction.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AplusCore.Runtime;
 using AplusCore.Types;
+using Microsoft.Scripting.Hosting;
 
 namespace AplusCoreUnitTests.Dlr
 {
@@ -16,6 +17,30 @@ namespace AplusCoreUnitTests.Dlr
         public void IncorrectTargetCall()
         {
             this.engine.Execute<AType>(" a:=6; a{1}");
+        }
+
+        [TestCategory("DLR"), TestCategory("InvokeFunction"), TestMethod]
+        public void FunctionCallInsideFunction()
+        {
+            ScriptScope scope = this.engine.CreateScope();
+
+            this.engine.Execute<AType>("f{x}: { x }", scope);
+            this.engine.Execute<AType>("g{}: { h := f; h{3} }", scope);
+
+            this.engine.Execute<AType>("g{}", scope);
+        }
+
+        [TestCategory("DLR"), TestCategory("InvokeFunction"), TestMethod]
+        public void FunctionCallInsiedFunction()
+        {
+            ScriptScope scope = this.engine.CreateScope();
+
+            this.engine.Execute<AType>("f{x}: { x }", scope);
+            this.engine.Execute<AType>("g{}: { h := f; eval 'h{3}' }", scope);
+
+            var result = this.engine.Execute<AType>("g{}", scope);
+
+            Assert.AreEqual<AType>(AInteger.Create(3), result);
         }
     }
 }
