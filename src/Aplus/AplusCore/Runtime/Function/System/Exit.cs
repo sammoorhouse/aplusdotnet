57b357912bbe6bf91cdbb4b9564940bd7cf8435f
galpeter <galpeter@inf.u-szeged.hu>
2011-06-27 12:40:23 +0000

    Patch by Janos Robert Gub (fixes issue 9).
    AplusCore:
     - Implemented the exit system function.
    
    AplusCoreUnitTests:
     - Testcases for exit system function.

---
+++ src/Aplus/AplusCore/Runtime/Function/System/Exit.cs
@@ -0,0 +1,37 @@
+using System;
+
+using AplusCore.Types;
+
+namespace AplusCore.Runtime.Function
+{
+    internal static partial class SystemFunction
+    {
+        /// <summary>
+        /// Exits with the exitcode given as parameter.
+        /// </summary>
+        /// <param nave="environment"></param>
+        /// <param name="input">exitcode</param>
+        [SystemFunction("_exit", "_exit{x}: exits with exitcode x")]
+        internal static AType Exit(AplusEnvironment environment, AType argument)
+        {
+            AType result;
+            bool isFirstScalar = argument.TryFirstScalar(out result, true);
+
+            if (isFirstScalar && !result.IsTolerablyWholeNumber || 
+                !(argument.Type == ATypes.AInteger || argument.Type == ATypes.AFloat))
+            {
+                throw new Error.Type("_exit");
+            }
+
+            if (!isFirstScalar)
+            {
+                throw new Error.Length("_exit");
+            }
+
+            Environment.Exit(result.asInteger);
+
+            // unreachable code
+            return argument;
+        }
+    }
+}
