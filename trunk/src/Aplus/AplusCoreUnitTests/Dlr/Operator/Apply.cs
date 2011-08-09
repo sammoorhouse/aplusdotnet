dc03012f0041a913c1d756a82184b7c7706ee035
galpeter@inf.u-szeged.hu <galpeter@inf.u-szeged.hu@f03d5aa0-f6a0-35d5-c5ef-26fb60699c29>
2011-08-09 14:51:17 +0000

    AplusCore:  - Quick fix: Allow general form for apply operator.

---
+++ trunk/src/Aplus/AplusCoreUnitTests/Dlr/Operator/Apply.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Scripting.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using AplusCore.Types;
+
+namespace AplusCoreUnitTests.Dlr.Operator
+{
+    [TestClass]
+    public class Apply : AbstractTest
+    {
+        [TestCategory("DLR"), TestCategory("General"), TestCategory("Apply"), TestMethod]
+        public void GeneralApplyInvoke()
+        {
+            ScriptScope scope = this.engine.CreateScope();
+            this.engine.Execute<AType>("f{a;b;c}: drop (a;b;c)", scope);
+            this.engine.Execute<AType>("boxF:= <{f}", scope);
+
+            AType expected = AArray.Create(ATypes.ABox,
+                ABox.Create(AInteger.Create(1)),
+                ABox.Create(AInteger.Create(2)),
+                ABox.Create(AInteger.Create(3))
+            );
+
+            AType result = this.engine.Execute<AType>("(boxF) each {1;2;3}", scope);
+
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual(InfoResult.OK, result.CompareInfos(expected));
+        }
+    }
+}
