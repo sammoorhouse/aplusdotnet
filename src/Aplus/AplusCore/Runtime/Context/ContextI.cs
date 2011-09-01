eed8580bb93144b8bed4151cce68f5dd9388f99a
galpeter <galpeter@inf.u-szeged.hu>
2011-09-01 10:15:41 +0000

    AplusCore:  - Refactor integer extraction the in i context.

--- src/Aplus/AplusCore/Runtime/Context/ContextI.cs
+++ src/Aplus/AplusCore/Runtime/Context/ContextI.cs
@@ -18,49 +18,49 @@ namespace AplusCore.Runtime.Context
         [AplusContextFunction("syncsend", "i.syncsend{scalar int;any;any} returns any")]
         public static AType SyncSend(Aplus environment, AType timeout, AType message, AType handle)
         {
-            int handleNumber = IsScalarAInteger(handle, "i.syncsend").asInteger;
+            int handleNumber = ExtractInteger(handle, "i.syncsend");
             return environment.GetService<AipcService>().SyncSend(handleNumber, message, timeout);
         }
 
         [AplusContextFunction("syncread", "i.syncread{scalar int;any} returns any")]
         public static AType SyncRead(Aplus environment, AType timeout, AType handle)
         {
-            int handleNumber = IsScalarAInteger(handle, "i.syncread").asInteger;
+            int handleNumber = ExtractInteger(handle, "i.syncread");
             return environment.GetService<AipcService>().SyncRead(handleNumber, timeout);
         }
 
         [AplusContextFunction("send", "i.send{scalar int;any} returns scalar int")]
         public static AType Send(Aplus environment, AType message, AType handle)
         {
-            int handleNumber = IsScalarAInteger(handle, "i.send").asInteger;
+            int handleNumber = ExtractInteger(handle, "i.send");
             return environment.GetService<AipcService>().Send(handleNumber, message);
         }
 
         [AplusContextFunction("open", "i.open{scalar int} returns scalar int")]
         public static AType Open(Aplus environment, AType handle)
         {
-            int handleNumber = IsScalarAInteger(handle, "i.open").asInteger;
+            int handleNumber = ExtractInteger(handle, "i.open");
             return environment.GetService<AipcService>().Open(handleNumber);
         }
 
         [AplusContextFunction("destroy", "i.destroy{scalar int} returns scalar int")]
         public static AType Destroy(Aplus environment, AType handle)
         {
-            int handleNumber = IsScalarAInteger(handle, "i.destroy").asInteger;
+            int handleNumber = ExtractInteger(handle, "i.destroy");
             return environment.GetService<AipcService>().Destroy(handleNumber);
         }
 
         [AplusContextFunction("close", "i.close{scalar int} returns scalar int")]
         public static AType Close(Aplus environment, AType handle)
         {
-            int handleNumber = IsScalarAInteger(handle, "i.close").asInteger;
+            int handleNumber = ExtractInteger(handle, "i.close");
             return environment.GetService<AipcService>().Close(handleNumber);
         }
 
         [AplusContextFunction("listenNPP", "i.listenNPP{any;any;scalar int;any} returns scalar int")]
         public static AType ListenNPP(Aplus environment, AType protocol, AType port, AType name, AType function)
         {
-            int portNumber = IsScalarAInteger(port, "i.listenNPP").asInteger;
+            int portNumber = ExtractInteger(port, "i.listenNPP");
 
             if (protocol.Type != ATypes.ASymbol || name.Type != ATypes.ASymbol || function.Type != ATypes.AFunc)
             {
@@ -97,7 +97,7 @@ namespace AplusCore.Runtime.Context
         public static AType ConnectNHPP(
             Aplus environment, AType protocol, AType port, AType host, AType name, AType function)
         {
-            int portNumber = IsScalarAInteger(port, "connectNHPP").asInteger;
+            int portNumber = ExtractInteger(port, "connectNHPP");
             return environment.GetService<AipcService>().Connect(function, name, host, portNumber, protocol);
         }
 
@@ -122,21 +122,21 @@ namespace AplusCore.Runtime.Context
         [AplusContextFunction("attrs", "i.attrs{scalar int} returns any")]
         public static AType Attributes(Aplus environment, AType handle)
         {
-            int handleNumber = IsScalarAInteger(handle, "i.attrs").asInteger;
+            int handleNumber = ExtractInteger(handle, "i.attrs");
             return environment.GetService<AipcService>().Attributes(handleNumber);
         }
 
         [AplusContextFunction("getattr", "i.getattr{scalar int;any} returns any")]
         public static AType GetAttribute(Aplus environment, AType attributeName, AType handle)
         {
-            int handleNumber = IsScalarAInteger(handle, "i.getattr").asInteger;
+            int handleNumber = ExtractInteger(handle, "i.getattr");
             return environment.GetService<AipcService>().GetAttribute(handleNumber, attributeName);
         }
 
         [AplusContextFunction("setattr", "i.setattr{scalar int;any;any} returns scalar int")]
         public static AType SetAttribute(Aplus environment, AType value, AType attributeName, AType handle)
         {
-            IsScalarAInteger(handle, "i.setattr");
+            ExtractInteger(handle, "i.setattr");
 
             return environment.GetService<AipcService>().SetAttribute(handle.asInteger, attributeName, value);
         }
@@ -144,7 +144,7 @@ namespace AplusCore.Runtime.Context
         [AplusContextFunction("whatis", "i.whatis{scalar int} returns any")]
         public static AType WhatIs(Aplus environment, AType handle)
         {
-            int handleNumber = IsScalarAInteger(handle, "i.whatis").asInteger;
+            int handleNumber = ExtractInteger(handle, "i.whatis");
             return environment.GetService<AipcService>().WhatIs(handleNumber);
         }
 
@@ -157,13 +157,13 @@ namespace AplusCore.Runtime.Context
         #region Utility
 
         /// <summary>
-        /// Checks if the argument is a valid scalar integer and returns it.
+        /// Returns the integer extracted from the argument.
         /// </summary>
         /// <param name="argument"></param>
         /// <param name="function"></param>
         /// <exception cref="Error.Type">If the argument is not a tolerably whole number.</exception>
         /// <exception cref="Error.Length">If the argument is not a one element array or scalar</exception>
-        private static AType IsScalarAInteger(AType argument, string function)
+        private static int ExtractInteger(AType argument, string function)
         {
             if (!argument.IsTolerablyWholeNumber)
             {
@@ -177,7 +177,7 @@ namespace AplusCore.Runtime.Context
                 throw new Error.Length(function);
             }
 
-            return result;
+            return result.asInteger;
         }
 
         #endregion
