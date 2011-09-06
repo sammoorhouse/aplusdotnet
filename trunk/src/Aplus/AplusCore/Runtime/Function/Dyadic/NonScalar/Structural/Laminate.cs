b038d9d7e6973b9d071267711d996573472a5567
galpeter@inf.u-szeged.hu <galpeter@inf.u-szeged.hu@f03d5aa0-f6a0-35d5-c5ef-26fb60699c29>
2011-09-06 17:47:08 +0000

    Original patch by Janos Robert Gub.
    AplusCore:
     - Refactored expand, laminate and replicate dyadic function.

--- trunk/src/Aplus/AplusCore/Runtime/Function/Dyadic/NonScalar/Structural/Laminate.cs
+++ trunk/src/Aplus/AplusCore/Runtime/Function/Dyadic/NonScalar/Structural/Laminate.cs
@@ -7,11 +7,36 @@ namespace AplusCore.Runtime.Function.Dyadic.NonScalar.Structural
 {
     class Laminate : AbstractDyadicFunction
     {
-        #region Variables
+        #region Laminate job info
 
-        private AType y;
-        private AType x;
-        private ATypes resultType;
+        class LaminateJobInfo
+        {
+            private AType left;
+            private AType right;
+            private ATypes resultType;
+
+            public LaminateJobInfo(ATypes resultType)
+            {
+                this.resultType = resultType;
+            }
+
+            internal AType Left
+            {
+                get { return this.left; }
+                set { this.left = value; }
+            }
+
+            internal AType Right
+            {
+                get { return this.right; }
+                set { this.right = value; }
+            }
+
+            internal ATypes ResultType
+            {
+                get { return this.resultType; }
+            }
+        }
 
         #endregion
 
@@ -19,95 +44,75 @@ namespace AplusCore.Runtime.Function.Dyadic.NonScalar.Structural
 
         public override AType Execute(AType right, AType left, Aplus environment = null)
         {
-            TypeCheckAndConvert(right, left);
-            return Compute();
+            LaminateJobInfo arguments = CreateLaminateJob(right, left);
+
+            return Compute(arguments);
         }
 
         #endregion
 
         #region Preparation
 
-        private void TypeCheckAndConvert(AType right, AType left)
-        {
-            AType temp_x = right;
-            AType temp_y = left;
-
-            if (temp_x.Type != temp_y.Type)
-            {
-                if (temp_y.Type == ATypes.AFloat && temp_x.Type == ATypes.AInteger)
-                {
-                    if (temp_x.IsArray)
-                    {
-                        temp_x.ConvertToFloat();
-                    }
-                    else
-                    {
-                        temp_x = AFloat.Create(temp_x.asFloat);
-                    }
-                }
-                else if (temp_y.Type == ATypes.AInteger && temp_x.Type == ATypes.AFloat)
-                {
-                    if (temp_y.IsArray)
-                    {
-                        temp_y.ConvertToFloat();
-                    }
-                    else
-                    {
-                        temp_y = AFloat.Create(temp_y.asFloat);
-                    }
-                }
-                else
-                {
-                    if (!Utils.IsSameGeneralType(left, right) && !Util.TypeCorrect(right.Type, left.Type, "N?", "?N"))
-                    {
-                        throw new Error.Type(TypeErrorText);
-                    }
-                }
-            }
-
-            resultType = temp_y.Length != 0 ? temp_y.Type : temp_x.Type;
-
-            PrepareVariables(temp_x, temp_y);
-        }
-
         /// <summary>
-        /// Set x,y attributes, if x or y is scalar, we reshape it to the other one`s shape.
-        /// At the end Rank, and Length error check.
         /// </summary>
         /// <param name="right"></param>
         /// <param name="left"></param>
-        private void PrepareVariables(AType right, AType left)
+        private LaminateJobInfo CreateLaminateJob(AType right, AType left)
         {
-            if(left.IsArray && !right.IsArray)
+            LaminateJobInfo laminateInfo = 
+                new LaminateJobInfo(left.Length != 0 ? left.Type : right.Type);
+            
+            if (left.IsArray && !right.IsArray)
             {
-                this.y = left.Clone();
-                this.x = DyadicFunctionInstance.Reshape.Execute(right, this.y.Shape.ToAArray());
+                laminateInfo.Left = left.Clone();
+                laminateInfo.Right = DyadicFunctionInstance.Reshape.Execute(right, left.Shape.ToAArray());
             }
             else if (!left.IsArray && right.IsArray)
             {
-                this.x = right.Clone();
-                this.y = DyadicFunctionInstance.Reshape.Execute(left, this.x.Shape.ToAArray());
+                laminateInfo.Right = right.Clone();
+                laminateInfo.Left = DyadicFunctionInstance.Reshape.Execute(left, right.Shape.ToAArray());
             }
             else
             {
-                this.x = right.Clone();
-                this.y = left.Clone();
+                laminateInfo.Right = right.Clone();
+                laminateInfo.Left = left.Clone();
             }
 
-            if (this.y.Rank != this.x.Rank)
+            // check if the types are same
+
+            if (right.Type != left.Type)
+            {
+                if (left.Type == ATypes.AFloat && right.Type == ATypes.AInteger)
+                {
+                    right = right.ConvertToFloat();
+                }
+                else if (left.Type == ATypes.AInteger && right.Type == ATypes.AFloat)
+                {
+                    left = left.ConvertToFloat();
+                }
+                else if (!Utils.IsSameGeneralType(left, right) && !Util.TypeCorrect(right.Type, left.Type, "N?", "?N"))
+                {
+                    throw new Error.Type(TypeErrorText);
+                }
+            }
+
+            // check the length, shape and rank
+            if (laminateInfo.Left.Rank != laminateInfo.Right.Rank)
             {
                 throw new Error.Rank(RankErrorText);
             }
 
-            if (!this.y.Shape.SequenceEqual(this.x.Shape))
+            if (!laminateInfo.Left.Shape.SequenceEqual(laminateInfo.Right.Shape))
             {
                 throw new Error.Length(LengthErrorText);
             }
 
-            if (this.y.Rank >= 9 || this.x.Rank >= 9)
+            if (laminateInfo.Left.Rank >= 9 || laminateInfo.Right.Rank >= 9)
             {
                 throw new Error.MaxRank(MaxRankErrorText);
             }
+
+            return laminateInfo;
         }
 
         #endregion
@@ -117,32 +122,30 @@ namespace AplusCore.Runtime.Function.Dyadic.NonScalar.Structural
         /// <summary>
         /// Compose left and right side.
         /// </summary>
+        /// <param name="laminateInfo">Instead of class variables.</param>
         /// <returns></returns>
-        private AType Compute()
+        private AType Compute(LaminateJobInfo laminateInfo)
         {
-            AType result = AArray.Create(ATypes.AArray);
+            AType result = AArray.Create(laminateInfo.ResultType);
 
-            if (this.y.Length == 0 || this.x.Length == 0)
+            if (laminateInfo.Left.Length == 0 || laminateInfo.Right.Length == 0)
             {
-                result.Type = this.y.Type = this.x.Type = resultType.MixedType() ? ATypes.ANull : this.resultType;
-            }
-            else
-            {
-                result.Type = this.resultType;
+                result.Type = laminateInfo.Left.Type = laminateInfo.Right.Type = 
+                    laminateInfo.ResultType.MixedType() ? ATypes.ANull : laminateInfo.ResultType;
             }
 
-            result.AddWithNoUpdate(this.y);
-            result.AddWithNoUpdate(this.x);
+            result.AddWithNoUpdate(laminateInfo.Left);
+            result.AddWithNoUpdate(laminateInfo.Right);
 
             result.Length = 2;
             result.Shape = new List<int>() { 2 };
 
-            if (this.x.Rank >= 1)
+            if (laminateInfo.Right.Rank >= 1)
             {
-                result.Shape.AddRange(this.x.Shape);
+                result.Shape.AddRange(laminateInfo.Right.Shape);
             }
 
-            result.Rank = 1 + this.x.Rank;
+            result.Rank = 1 + laminateInfo.Right.Rank;
 
             return result;
         }
