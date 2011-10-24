6eddcb437d77eced4bf7aeddd412141837690007
galpeter <galpeter@inf.u-szeged.hu>
2011-10-24 15:41:34 +0000

    AplusCore:  - Added extension method to proved a more intuitive(?) way to build DLR.Call expressions.

--- src/Aplus/AplusCore/Compiler/ExpressionTreeExtensions.cs
+++ src/Aplus/AplusCore/Compiler/ExpressionTreeExtensions.cs
@@ -19,6 +19,23 @@ namespace AplusCore.Compiler
         }
 
         /// <summary>
+        /// Build <see cref="DLR.MethodCallExperssion"/> from the given arguments.
+        /// </summary>
+        /// <remarks>
+        /// This is a shorthand for a simple <see cref="DLR.Experssion.Call"/> invokation.
+        /// </remarks>
+        /// <typeparam name="T">The type of the instance.</typeparam>
+        /// <param name="instance">The instance on which the method will be called.</param>
+        /// <param name="methodName">The name of the method in the given type.</param>
+        /// <param name="arguments">The additional arguments for the method call.</param>
+        /// <returns><see cref="DLR.MethodCallExperssion"/> built using the parameters.</returns>
+        internal static DLR.MethodCallExpression Call<T>(
+            this DLR.Expression instance, string methodName, params DLR.Expression[] arguments)
+        {
+            return DLR.Expression.Call(instance, typeof(T).GetMethod(methodName), arguments);
+        }
+
+        /// <summary>
         /// Wraps the expression inside a Dynamic cast to AType
         /// </summary>
         /// <param name="expression">Expression to cast.</param>
