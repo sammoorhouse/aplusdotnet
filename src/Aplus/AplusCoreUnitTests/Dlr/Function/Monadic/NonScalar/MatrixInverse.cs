8ad128b5e028b2e8fbe8f87e0376d6bc7249cddf
galpeter <galpeter@inf.u-szeged.hu>
2011-11-04 13:28:30 +0000

    Patch By Janos Robert Gub.
    AplusCore:
     - Implemented pseudo inverse for matrix inverse function.
    
    AplusCoreUnitTests:
     - Added testcases for pseudo inverse.

--- src/Aplus/AplusCoreUnitTests/Dlr/Function/Monadic/NonScalar/MatrixInverse.cs
+++ src/Aplus/AplusCoreUnitTests/Dlr/Function/Monadic/NonScalar/MatrixInverse.cs
@@ -1,3 +1,5 @@
+using System;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using AplusCore.Runtime;
@@ -31,12 +33,33 @@ namespace AplusCoreUnitTests.Dlr.Function.Monadic.NonScalar
         [TestCategory("DLR"), TestCategory("Monadic"), TestCategory("MatrixInverse"), TestMethod]
         public void Rank2Test()
         {
-            AType expected = AArray.Create(ATypes.AFloat, 
-                                           AArray.Create(ATypes.AFloat,AFloat.Create(1),AFloat.Create(-1)),
-                                           AArray.Create(ATypes.AFloat,AFloat.Create(-0.5),AFloat.Create(1))
+            AType expected = AArray.Create(ATypes.AFloat,
+                                           AArray.Create(ATypes.AFloat, AFloat.Create(1), AFloat.Create(-1)),
+                                           AArray.Create(ATypes.AFloat, AFloat.Create(-0.5), AFloat.Create(1))
                                            );
             AType result = this.engine.Execute<AType>("mdiv 2 2 rho 2 2 1 2");
 
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCategory("DLR"), TestCategory("Monadic"), TestCategory("MatrixInverse"), TestMethod]
+        public void PseudoInverseTest()
+        {
+            AType expected = AArray.Create(ATypes.AFloat,
+                                           AArray.Create(ATypes.AFloat, AFloat.Create(-1.3333), AFloat.Create(-0.3333), AFloat.Create(0.6667)),
+                                           AArray.Create(ATypes.AFloat, AFloat.Create(1.0833), AFloat.Create(0.3333), AFloat.Create(-0.4167))
+                                           );
+
+            AType result = this.engine.Execute<AType>("mdiv 3 2 rho 1 2 3 4 5 6");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result[i][j] = AFloat.Create(Math.Round(result[i][j].asFloat, 4));
+                }
+            }
+            
             Assert.AreEqual(expected, result);
         }
 
@@ -63,6 +86,20 @@ namespace AplusCoreUnitTests.Dlr.Function.Monadic.NonScalar
         public void SingularCase()
         {
             this.engine.Execute<AType>("mdiv 2 2 rho 1");
+        }
+
+        [TestCategory("DLR"), TestCategory("Monadic"), TestCategory("MatrixInverse"), TestMethod]
+        [ExpectedException(typeof(Error.Domain))]
+        public void SingularNotSquareMatrixCase1()
+        {
+            this.engine.Execute<AType>("mdiv 5 4 rho 1 0 0 0 0 0 0 4 0 3 0 0 0 0 0 0 2 0 0 0");
+        }
+
+        [TestCategory("DLR"), TestCategory("Monadic"), TestCategory("MatrixInverse"), TestMethod]
+        [ExpectedException(typeof(Error.Domain))]
+        public void SingularNotSquareMatrixCase2()
+        {
+            this.engine.Execute<AType>("mdiv 4 3 rho 2 -4 5 6 0 3 2 -4 5 6 0 3");
         }
 
         #endregion
