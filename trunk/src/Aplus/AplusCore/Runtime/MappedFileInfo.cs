ddef9cc622c0c619ff97b9e91aa4c86bf7865ceb
galpeter@inf.u-szeged.hu <galpeter@inf.u-szeged.hu@f03d5aa0-f6a0-35d5-c5ef-26fb60699c29>
2012-01-25 10:45:18 +0000

    Patch by Janos Robert Gub.
    AplusCore:
      - Optimalized memory usage of memory mapped files.
      - Extracted memory mapped position information to a separate file.

---
+++ trunk/src/Aplus/AplusCore/Runtime/MappedFileInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AplusCore.Runtime
+{
+    internal static class MappedFileInfo
+    {
+        public static readonly byte TypePosition = 4;
+        public static readonly byte RankPosition = 8;
+        public static readonly byte ItemCountPosition = 12;
+        public static readonly byte ShapePosition = 16;
+        public static readonly byte LeadingAxesPosition = 52;
+
+        public static readonly int ByteSize = Marshal.SizeOf(typeof(byte));
+        public static readonly int IntSize = Marshal.SizeOf(typeof(int));
+        public static readonly int DoubleSize = Marshal.SizeOf(typeof(double));
+
+        public static readonly int HeaderSize = 14 * IntSize;
+    }
+}
